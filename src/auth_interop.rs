d2e4547562006ea6790d41d4eee4ab45f8eef0ab
Hajime Oh-yake <vj.valencia@me.com>
2026-02-16 01:22:27 +0900

    Fix: Set public_url to /leaf/ in Trunk.toml and kept module paths as project-root-relative (/auth.js) (v0.5.15)

--- src/auth_interop.rs
+++ src/auth_interop.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::closure::Closure;
 
-#[wasm_bindgen(module = "auth.js")]
+#[wasm_bindgen(module = "/auth.js")]
 extern "C" {
     pub fn init_google_auth(client_id: &str, callback: &Closure<dyn FnMut(String)>);
     pub fn request_access_token();
